2a223b3ee70385524902d0508b576624b07d9a5f
adrixop95 <adrixop95@me.com>
2019-01-15 20:33:54 +0100

    dragon curve | readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Generowanie_krzywych_fraktalnych
-Projekt zaliczeniowych z zajęć z grafiki | WIiNoM UŚ
+# Generowanie krzywych fraktalnych
+Projekt zaliczeniowych z zajęć z grafiki | WIiNoM UŚ  
+
+W zasadzie w całości bazowany na [tym](https://github.com/carterturn/opengl_fractals) repozytorium użytkownika [carterturn](https://github.com/carterturn).
+
+# Wymagane biblioteki dla systemu Ubuntu GNU/Linux
+- libglfw3-dev 
+- libglfw3 
+- freeglut3-dev 
+- mesa-utils
+
+## Instalacja bibliotek:
+```
+sudo apt-get install libglfw3-dev libglfw3 freeglut3-dev mesa-utils
+```  
+
+Made with 😺 and 💝,  
+by Adrian Rupala
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
+#include <chrono>
 #include <iostream>
-#include <fstream>
 #include <random>
-
-// libglfw3-dev libglfw3 debian apt pkgs
+#include <thread>
+#include <fstream>
 #include <GLFW/glfw3.h>
 #include <GL/gl.h>
 
@@ -10,82 +10,133 @@ using namespace std;
 
 #define WIDTH 1280
 #define HEIGHT 720
+#define PI 3.14159265358
+
+int max_depth;
 
 struct point{
-		double x;
-		double y;
+		double x, y;
 };
 
-int koch_curve(){
-		int max_depth = 1;
-        	
-		glfwInit();
-		GLFWwindow * window = glfwCreateWindow(WIDTH, HEIGHT, "Krzywa Kocha", NULL, NULL);
+void koch_curve(){       
 
-		glfwMakeContextCurrent(window);
-		glOrtho(0, WIDTH, 0, HEIGHT, -1.0, 1.0);
+	cout << "Podaj maksymalne zagnieżdżenie " << endl;
+	cin >> max_depth;
 
-		double side_length = (HEIGHT * 3.0/2.0) / sqrt(3.0);
-		double width_offset = (WIDTH - side_length) / 2.0;
-
-		while(true){
-				glClear(GL_COLOR_BUFFER_BIT);
-				glColor3f(0.0f, 1.0f, 0.0f);
-				glBegin(GL_LINES);
-				vector<point> points;
-
-				points.push_back({.x = width_offset, .y = HEIGHT/4.0});
-				points.push_back({.x = WIDTH - width_offset, .y = HEIGHT/4.0});
-				points.push_back({.x = WIDTH/2, .y = HEIGHT});
+	glfwInit();
+	GLFWwindow * window = glfwCreateWindow(WIDTH, HEIGHT, "Krzywa Kocha", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	glOrtho(0, WIDTH, 0, HEIGHT, -1.0, 1.0);
+	double side_length = (HEIGHT * 3.0/2.0) / sqrt(3.0);
+	double width_offset = (WIDTH - side_length) / 2.0;
+	
+	while(true){
+		glClear(GL_COLOR_BUFFER_BIT);
+		glColor3f(0.0f, 1.0f, 0.0f);
+		glBegin(GL_LINES);
+		vector<point> points;
+		points.push_back({.x = width_offset, .y = HEIGHT/4.0});
+		points.push_back({.x = WIDTH - width_offset, .y = HEIGHT/4.0});
+		points.push_back({.x = WIDTH/2, .y = HEIGHT});
 				
-                for(int d = 0; d < max_depth; d++){
-						vector<point> new_points;
+		for(int d = 0; d < max_depth; d++){
+			vector<point> new_points;
+			
+			for(int i = 0; i < points.size(); i++){
+				double x1 = points[i].x;
+				double x2 = points[(i+1)%points.size()].x;
+				double y1 = points[i].y;
+				double y2 = points[(i+1)%points.size()].y;
+								
+				double rx1 = (x2 - x1) / 3.0 + x1;
+				double ry1 = (y2 - y1) / 3.0 + y1;
+				double rx2 = 2.0 * (x2 - x1) / 3.0 + x1;
+				double ry2 = 2.0 * (y2 - y1) / 3.0 + y1;
+				new_points.push_back(points[i]);
+				new_points.push_back({.x = rx1, .y = ry1});
 
-						for(int i = 0; i < points.size(); i++){
-								double x1 = points[i].x;
-								double x2 = points[(i+1)%points.size()].x;
-								double y1 = points[i].y;
-								double y2 = points[(i+1)%points.size()].y;
-                                
-								double rx1 = (x2 - x1) / 3.0 + x1;
-								double ry1 = (y2 - y1) / 3.0 + y1;
-								double rx2 = 2.0 * (x2 - x1) / 3.0 + x1;
-								double ry2 = 2.0 * (y2 - y1) / 3.0 + y1;
-
-								new_points.push_back(points[i]);
-								new_points.push_back({.x = rx1, .y = ry1});
-
-								if(abs(ry1 - ry2) < 0.01){
-										new_points.push_back({.x = (rx2 - rx1) / 2.0 + rx1, .y = ry1 + (rx1 - rx2) * sqrt(3.0) / 2.0});
-								}
-								else if((x2 > x1) == (y1 > y2)){
-										new_points.push_back({.x = rx1 + (rx1 - rx2), .y = ry2});
-								}
-								else{
-										new_points.push_back({.x = rx2 + (rx2 - rx1), .y = ry1});
-								}
-
-								new_points.push_back({.x = rx2, .y = ry2});
-						}
-                        
-						points = new_points;
+				if(abs(ry1 - ry2) < 0.01){
+					new_points.push_back({.x = (rx2 - rx1) / 2.0 + rx1, .y = ry1 + (rx1 - rx2) * sqrt(3.0) / 2.0});
 				}
-
-				glVertex2i(points[points.size()-1].x, points[points.size()-1].y);
-				
-                for(int i = 0; i < points.size(); i++){
-						glVertex2i(points[i].x, points[i].y);
-						glVertex2i(points[i].x, points[i].y);
+				else if((x2 > x1) == (y1 > y2)){
+					new_points.push_back({.x = rx1 + (rx1 - rx2), .y = ry2});
 				}
-
-				glVertex2i(points[0].x, points[0].y);
-				glEnd();
-
-				glfwSwapBuffers(window);
+				else{
+					new_points.push_back({.x = rx2 + (rx2 - rx1), .y = ry1});
+				}
+				new_points.push_back({.x = rx2, .y = ry2});
+			}
+						
+			points = new_points;
 		}
+
+		glVertex2i(points[points.size()-1].x, points[points.size()-1].y);
+		
+		for(int i = 0; i < points.size(); i++){
+			glVertex2i(points[i].x, points[i].y);
+			glVertex2i(points[i].x, points[i].y);
+		}
+
+		glVertex2i(points[0].x, points[0].y);
+		glEnd();
+		glfwSwapBuffers(window);
+	}
 }
 
-int main() {
+void dragon_curve(){
+	cout << "Podaj maksymalne zagnieżdżenie " << endl;
+	cin >> max_depth;
+	glfwInit();
+	GLFWwindow * window = glfwCreateWindow(WIDTH, HEIGHT, "Smok Heighwaya", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	glOrtho(0, WIDTH, 0, HEIGHT, -1.0, 1.0);
+
+	while(true){
+		glClear(GL_COLOR_BUFFER_BIT);
+		glColor3f(0.0f, 1.0f, 0.0f);
+		glBegin(GL_LINES);
+		chrono::high_resolution_clock::time_point start = chrono::high_resolution_clock::now();
+		vector<point> points;
+		points.push_back({.x = WIDTH/4.0, .y = HEIGHT/2.0});
+		points.push_back({.x = WIDTH/2.0, .y = HEIGHT/4.0});
+		points.push_back({.x = WIDTH*3.0/4.0, .y = HEIGHT/2.0});
+		double length = sqrt((WIDTH * WIDTH / 16.0) + (HEIGHT * HEIGHT / 16.0));
+		
+		for(int d = 0; d < max_depth; d++){
+			length = length / sqrt(2.0);
+			vector<point> new_points;	
+
+			for(int i = 0; i < points.size() - 1; i++){
+				double x1 = points[i].x;
+				double y1 = points[i].y;
+				double theta = atan2(points[i+1].y - y1, points[i+1].x - x1);
+				double length = sqrt(pow(points[i+1].x - x1, 2.0) + pow(points[i+1].y - y1, 2.0)) / sqrt(2.0);
+				double delta_angle = ((i % 2) == 0 ? (PI / 4.0) : -(PI / 4.0));
+				double rx = x1 + length * cos(theta + delta_angle);
+				double ry = y1 + length * sin(theta + delta_angle);
+				new_points.push_back(points[i]);
+				new_points.push_back({.x = rx, .y = ry});
+			}
+
+			new_points.push_back(points[points.size() - 1]);
+			points = new_points;
+		}
+		
+		glVertex2i(points[0].x, points[0].y);
+		
+		for(int i = 1; i < points.size() - 1; i++){
+			glVertex2i(points[i].x, points[i].y);
+			glVertex2i(points[i].x, points[i].y);
+		}
+		
+		glVertex2i(points[points.size()-1].x, points[points.size()-1].y);
+		glEnd();
+		glfwSwapBuffers(window);
+		this_thread::sleep_for(chrono::milliseconds(100));
+	}
+}
+
+int main(int argc, char * argv[]) {
 	int user_choice;
 	bool is_running = true;
 	  
@@ -97,7 +148,7 @@ int main() {
 
 		// Opcje wyboru
 		cout << "\n1 - Krzywa Kocha (Koch snowflake)" << endl;
-		cout << "2 - Krzywa a" << endl;
+		cout << "2 - Smok Heighwaya (Dragon curve)" << endl;
 		cout << "3 - Krzywa a" << endl;
 		cout << "4 - Krzywa a" << endl;
 		cout << "5 - Krzywa a" << endl;
@@ -111,6 +162,7 @@ int main() {
             koch_curve();
 		}
 		else if (user_choice == 2) {
+			dragon_curve();
         }
 		else if (user_choice == 3) {
 		}
